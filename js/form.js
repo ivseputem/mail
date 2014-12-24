5c3ab8fe71757e26378e57e3b222220a8caec95c
Alekasandr <uralkid@gmail.com>
2014-12-25 03:28:05 +0600

    update

--- js/form.js
+++ js/form.js
@@ -1,27 +1,27 @@
 $(function(){
-	$('form').validate({
-		rules: {
-			'user_name' : {
-				required:true
-			},
-			'user_phone' : {
-				required:true
-			},
-			'user_email': {
-				email:true
-			}
-		},
-		errorPlacement: function(error, element) {},
-		submitHandler: function(form) {
-						
-			$.post('imail.php', $(form).serialize(), function(result){
-				$('.success').fadeIn(300, function() {
-					$(this).fadeOut(10000);
-					$(form).trigger('reset');
-				});
-			});
-			
-			return false;
-		}
-	});
+    $('form').validate({
+        rules: {
+            'user_name' : {
+                required:true
+            },
+            'user_phone' : {
+                required:true
+            },
+            'user_email': {
+                email:true
+            }
+        },
+        errorPlacement: function(error, element) {},
+        submitHandler: function(form) {
+                        
+            $.post('imail.php', $(form).serialize(), function(result){
+                $('.success').fadeIn(300, function() {
+                    $(this).fadeOut(10000);
+                    $(form).trigger('reset');
+                });
+            });
+            
+            return false;
+        }
+    });
 })
