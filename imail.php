29a90f12d768463be70fc29c15bf96759ff7402c
Alekasandr <uralkid@gmail.com>
2014-12-25 01:34:33 +0600

    create

---
+++ imail.php
@@ -0,0 +1,38 @@
+<?php
+/*development*/
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+
+/*production*/
+//error_reporting(0);
+//ini_set('display_errors', 'Off');
+
+if(empty($_POST) && ($_SERVER['HTTP_X_REQUESTED_WITH'] !== 'XMLHttpRequest')) exit;
+
+
+$email = 'example@example.com';
+$subject = 'New message';
+$message = '';
+$from = 'example@example.com';
+
+$headers = "From: ".$from."\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=utf-8\r\n";
+
+$fields = array
+(
+	'user_name'		=> 'Name: ',
+	'user_phone'	=> 'Phone: ',
+	'user_email'	=> 'E-mail: ',
+);
+
+foreach($_POST as $field => $value)
+(
+	if(!empty($fields[$field]))
+		$message .= $fields[$field] . $value . '<br/>';
+)
+
+if(mail($email, $subject, $message, $headers))
+	echo json_encode(array('error' => false));
+else
+	echo json_encode(array('error' => true));
